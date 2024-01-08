125fdd777b3c4999cd28fc38398e89e47942d463
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2024-01-08 20:49:25 +0200

    first commit

---
+++ src/components/FeatureCard.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import styles from '../style';
+
+const FeatureCard = ({ icon, title, content, index, features }) => (
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? 'mb-6' : 'mb-0'
+    } feature-card`}
+  >
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+    >
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain " />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[23px] mb-1">
+        {content}
+      </p>
+    </div>
+  </div>
+);
+
+FeatureCard.propTypes = {
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  features: PropTypes.array.isRequired,
+};
+
+export default FeatureCard;
